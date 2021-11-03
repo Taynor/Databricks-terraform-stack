e9fed5dc53a21c2326ccdaaa29991841593bb381
Taynor <59668937+Taynor@users.noreply.github.com>
2021-11-03 14:49:44 +0000

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "location" {
+    type    = string
+    default = "uksouth"
+}
+variable "rgname" {
+    type    = string
+    default = "databricksDev"
+}
+variable "workspacename" {
+    type    = string
+    default = "devWorkSpaceOps"
+}
+variable "mrgworkspace" {
+    type    = string
+    default = "mg-work-dev"
+}
+variable "sku" {
+    type    = string
+    default = "premium"
+}
